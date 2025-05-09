45c806e60f7a7378e0293bb80728c3a36ae1290d
FAST-lab <jfsmrewm@gmail.com>
2025-05-09 03:24:30 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,12 @@
 from .data_utils import CodeEmbedder, clean_code
+from .model_downloader import download_and_extract_models
+from .classifier import load_saved_models, stacked_prediction, predict_vulnerabilities
 
-__all__ = ['CodeEmbedder', 'clean_code']
+__all__ = [
+    'CodeEmbedder',
+    'clean_code',
+    'download_and_extract_models',
+    'load_saved_models',
+    'stacked_prediction',
+    'predict_vulnerabilities'
+]
