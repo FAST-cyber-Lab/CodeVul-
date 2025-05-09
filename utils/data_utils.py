dae582643ad0aeb8a746433ac86abb3478f9a149
FAST-lab <jfsmrewm@gmail.com>
2025-05-09 03:23:49 -0400

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -3,13 +3,24 @@ import numpy as np
 import re
 from transformers import AutoModel, AutoTokenizer
 
-def clean_code(code):
+def clean_code(code_str):
 
-    code = re.sub(r'/\*.*?\*/', '', code, flags=re.DOTALL)
-    code = re.sub(r'//.*?$', '', code, flags=re.MULTILINE)
-    code = re.sub(r'^\s*[\n\r]', '', code, flags=re.MULTILINE)
+    if not isinstance(code_str, str):
+        return ""
     
-    return code.strip()
+
+    code_str = re.sub(r'#.*$', '', code_str, flags=re.MULTILINE)
+    code_str = re.sub(r'\/\/.*$', '', code_str, flags=re.MULTILINE)
+    code_str = re.sub(r'\/\*[\s\S]*?\*\/', '', code_str)
+    
+    code_str = re.sub(r'\s+', ' ', code_str)
+    
+
+    code_str = re.sub(r';\s*', ';\n', code_str)
+    code_str = re.sub(r'{\s*', '{\n', code_str)
+    code_str = re.sub(r'}\s*', '}\n', code_str)
+    
+    return code_str.strip()
 
 class CodeEmbedder:
     def __init__(self, model_name="microsoft/graphcodebert-base", device=None):
@@ -18,19 +29,19 @@ class CodeEmbedder:
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model.to(self.device)
     
-    def embed_code_snippets(self, batch_code, max_length=None):
-
-        cleaned_batch_code = [clean_code(code) for code in batch_code]
+    def embed_code_snippets(self, batch_code, max_length=None, clean=True):
+        if clean:
+            batch_code = [clean_code(code) for code in batch_code]
         
         if max_length is None:
-            token_lengths = [len(self.tokenizer.encode(code, truncation=False)) for code in cleaned_batch_code]
+            token_lengths = [len(self.tokenizer.encode(code, truncation=False)) for code in batch_code]
             max_length = int(np.percentile(token_lengths, 95))
         
         max_length = min(max_length, self.tokenizer.model_max_length)
         
         with torch.no_grad():
             tokenized_code = self.tokenizer(
-                cleaned_batch_code,
+                batch_code,
                 return_tensors="pt",
                 padding="max_length",
                 truncation=True,
